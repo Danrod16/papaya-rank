36a223579f24571783c0eeada70e4e6ad5488aec
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-12-30 15:46:51 +0100

    Feat: added front end and global ranking JS

--- background.js
+++ background.js
@@ -1,17 +1,26 @@
     console.log('clicked')
     const button = document.getElementById('rank')
     const ranking = document.getElementById('result')
+    const loader = document.getElementById('loader')
+    const globalRanking = document.getElementById('global_ranking')
     button.addEventListener('click', async () => {
         console.log("clicking")
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            loader.style.display = 'block';
             const activeTab = tabs[0];
-            const domain = activeTab.url.replace(/^(http|https):\/\//, '').replace(/\/$/, '')
+            console.log(activeTab.url)
+            const domain = activeTab.url.match(/\/\/.*?(?=\/)/)[0].replace('//', '')
             console.log(domain)
             fetch(`https://openpagerank.com/api/v1.0/getPageRank?domains%5B%5D=${domain}`, { method: 'GET', headers: { 'Content-Type': 'application/json', 'API-OPR': 'gkcsw8kws84wogoow4go0gwss0ckwkwsk8swwsk8' }
         }
             ).then(response => response.json() )
             .then(result => 
-                ranking.innerHTML = result.response[0].page_rank_decimal
+                setTimeout(() => {
+                    loader.style.display = 'none',
+                    ranking.innerHTML = result.response[0].page_rank_decimal,
+                    globalRanking.innerHTML = `<strong>Global ranking:</strong> #${result.response[0].rank}`
+                }, 1900),
+              
             );
         });
     });
