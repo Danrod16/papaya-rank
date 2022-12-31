4e9ccbc930c2017be793f7f025c5ddc362a10d18
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-12-31 11:56:18 +0100

    Refactored code

--- background.js
+++ background.js
@@ -4,23 +4,32 @@
     const loader = document.getElementById('loader')
     const globalRanking = document.getElementById('global_ranking')
     button.addEventListener('click', async () => {
-        console.log("clicking")
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             loader.style.display = 'block';
             const activeTab = tabs[0];
-            console.log(activeTab.url)
             const domain = activeTab.url.match(/\/\/.*?(?=\/)/)[0].replace('//', '')
-            console.log(domain)
             fetch(`https://openpagerank.com/api/v1.0/getPageRank?domains%5B%5D=${domain}`, { method: 'GET', headers: { 'Content-Type': 'application/json', 'API-OPR': 'gkcsw8kws84wogoow4go0gwss0ckwkwsk8swwsk8' }
         }
             ).then(response => response.json() )
             .then(result => 
-                setTimeout(() => {
-                    loader.style.display = 'none',
-                    ranking.innerHTML = result.response[0].page_rank_decimal,
-                    globalRanking.innerHTML = `<strong>Global ranking:</strong> #${result.response[0].rank}`
-                }, 1900),
-              
+                setTimeout(() => {displayResult(result.response[0])}, 1900)
             );
         });
     });
+
+    function displayResult(globalRank) {
+        setColor(globalRank),
+        loader.style.display = 'none',
+        ranking.innerHTML = `${globalRank.page_rank_decimal}<small>/10</small>`,
+        globalRanking.innerHTML = `<strong>Global ranking:</strong> #${globalRank.rank}`
+    }
+
+    function setColor(globalRank) {
+        if (globalRank.page_rank_decimal < 2) {
+            ranking.style.color = "#f25555";
+        } else if (globalRank.page_rank_decimal > 2 && globalRank.page_rank_decimal < 5) {
+            ranking.style.color = "#e1800b";
+        } else {
+            ranking.style.color = "#4caf50";
+        }
+    }
